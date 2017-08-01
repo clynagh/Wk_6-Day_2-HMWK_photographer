b2f0a76fc22e43ef3744f4b34745a1cb833b9490
Craig Lynagh <craiglynagh@gmail.com>
2017-08-01 15:31:04 +0100

    Intitial Commit

---
+++ Photographer.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Photographer{
+  private String name;
+
+
+
+  public Photographer(){
+    this.name = name;
+
+  }
+
+}
---
+++ PhotographerTest.java
@@ -0,0 +1,16 @@
+import static rg.junit.Assert.*;
+import org.junit.*;
+
+public class PhotographerTest {
+
+Photographer photographer;
+
+
+@Before
+public before void(){
+  photographer = new Photographer();
+}
+
+
+
+}
